506dda24142904ee1a068b4e3d3ac4326313a98a
venderjin <dydgh3331@naver.com>
2025-05-29 12:11:47 +0900

    indet tabwidth 2

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
-    semi: false, // 세미콜론 제거
-    singleQuote: true, // 작은따옴표 사용
-    trailingComma: 'all', // 마지막 쉼표 유지
-    printWidth: 120, // 줄바꿈 기준
-    tabWidth: 4, // 들여쓰기
-    plugins: ['prettier-plugin-tailwindcss'], // Tailwind class 정렬
+  semi: false, // 세미콜론 제거
+  singleQuote: true, // 작은따옴표 사용
+  trailingComma: 'all', // 마지막 쉼표 유지
+  printWidth: 120, // 줄바꿈 기준
+  tabWidth: 2, // 들여쓰기
+  plugins: ['prettier-plugin-tailwindcss'], // Tailwind class 정렬
 }
