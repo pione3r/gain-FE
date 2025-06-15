e385d19d9f3d1aeb1afed1667fb697dfc361071b
junhyunyong <junhyun.yong@docenty.ai>
2025-06-15 17:03:01 +0900

    fix header logo path

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="flex w-full items-center justify-between bg-white px-6 py-4 md:px-10 lg:px-20">
       <Link href="/" className="mr-10">
-        <Image src="/logo.png" alt="GAIN 로고" width={120} height={50} />
+        <Image src="/logo.svg" alt="GAIN 로고" width={120} height={50} />
       </Link>
       {/* <nav className="hidden md:flex space-x-8">
         <Link href="/service" className="text-gray-700 hover:text-gray-900 font-medium">
